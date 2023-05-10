7a3cd2093d15ca6039d54df98f883fb820b3007f
Andrew Zhu <me@andrew-zhu.com>
2023-05-10 18:23:52 -0400

    fix(utils): openai cors middleware

--- fastlmi/utils.py
+++ fastlmi/utils.py
@@ -6,5 +6,7 @@ def cors_allow_openai(app):
     Add CORS middleware to an app to allow requests from chat.openai.com (for localhost plugins).
     This utility is provisional.
     """
-    app.add_middleware(CORSMiddleware, allow_origins=["https://chat.openai.com"], allow_methods=["*"])
+    app.add_middleware(
+        CORSMiddleware, allow_origins=["https://chat.openai.com"], allow_methods=["*"], allow_headers=["*"]
+    )
     return app
